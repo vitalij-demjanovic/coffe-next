6f7514f188e3ffca7eee3e1b3c0def516c434da1
vitalij_demjanovic <vitalij.demjanovic@maisondidee.com>
2023-11-26 23:19:55 +0100

    DELETE cache swell

--- lib/swell/products.ts
+++ lib/swell/products.ts
@@ -2,38 +2,36 @@ import { cache } from 'react';
 import swell from '@/lib/swell/client';
 import { GetProductsInput } from '@/lib/types/products';
 
-export const getProducts = cache(
-    async ({
-        page = 1,
-        sort = '',
-        search = '',
-        categories = [],
-        limit = 25
-    }: GetProductsInput) => {
-        const query = {
-            page,
-            limit,
-            sort,
-            search,
-            categories,
-            expand: ['variants', 'categories']
-        };
+export const getProducts = async ({
+    page = 1,
+    sort = '',
+    search = '',
+    categories = [],
+    limit = 25
+}: GetProductsInput) => {
+    const query = {
+        page,
+        limit,
+        sort,
+        search,
+        categories,
+        expand: ['variants', 'categories']
+    };
 
-        return await swell.products.list(query);
-    }
-);
+    return await swell.products.list(query);
+};
 
-export const getProductSlugOrId = cache(async (slugOrID: string) => {
+export const getProductSlugOrId = async (slugOrID: string) => {
     return await swell.products.get(slugOrID);
-});
+};
 
-export const listCategories = cache(async (limit = 25, page = 1) => {
+export const listCategories = async (limit = 25, page = 1) => {
     return await swell.categories.list({
         limit,
         page,
         expand: ['products']
     });
-});
+};
 
 export const sortMap = new Map([
     ['latest', ''],
